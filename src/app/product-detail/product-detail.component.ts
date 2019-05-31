671d06c9e252093132cc01e789d3783e788bfaee
Scott Wren <cynics0xff@gmail.com>
2019-05-31 15:18:52 -0400

    Add method for getting product detail data from the api

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../models/product';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  product: Product = { id: 0, prod_name: '', prod_desc: '', prod_price: null, updated_at: null };
+  isLoading = true;
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
 
   ngOnInit() {
+    this.getProductDetails(this.route.snapshot.params['id']);
   }
 
+  getProductDetails(id) {
+    this.api.getProduct(id)
+    .subscribe(data => {
+      this.product = data;
+      
+      //debug
+      console.log(this.product);
+
+      this.isLoading = false;
+    });
+  }
 }
