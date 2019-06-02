ac176381f89d7231b9d2e131fbf6af9b4c63ddb9
Scott Wren <cynics0xff@gmail.com>
2019-06-01 22:23:24 -0400

    Add method for getting product-edit details from the api service

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../models/product';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-product-edit',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  product: Product = { id: 0, prod_name: '', prod_desc: '', prod_price: null, updated_at: null }
+  isLoading = true;
+
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) {
+    
+   }
 
   ngOnInit() {
+    this.getProductDetails(this.route.snapshot.params['id']);
+  }
+
+  getProductDetails(id) {
+    this.api.getProduct(id)
+    .subscribe(res => {
+      this.product = res
+
+      console.log(this.product);
+      this.isLoading = false;
+    });
   }
 
 }
